5c8b5a244ec960a89a42d2ada5b119140d8c2ca2
Jardel Ferreira <jardel.ferreira@unifesp.br>
2026-01-15 19:51:52 -0300

    ok

--- clusteriação/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from utils.st_functions import sh, input_checker, show_result, get_prev_results_infos
-from utils.inputs_handler import build_training_df
-from utils.ml_scripts import get_afinidade_df
-from utils.po_scripts import get_results
